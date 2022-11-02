de72ab659596b76de52a9a0cd586b788d379292e
qkr2435 <qkr2435@gmail.com>
2022-11-02 23:59:44 +0900

    Iterable 구현

--- src/main/java/com/api/service/ChargePointService.java
+++ src/main/java/com/api/service/ChargePointService.java
@@ -1,8 +1,14 @@
 package com.api.service;
 
+import com.api.entity.ChargePointEntity;
+
+import java.util.Optional;
+
 public interface ChargePointService {
     //CP CRUD 구현
     public void createChargePoint();
     public void deleteChargePointById(String id);
-
+    //Iterable , Optional 차이 정리
+    public Iterable<ChargePointEntity> getAllChargerPoint();
+    public Optional<ChargePointEntity> getChargePointById();
 }
