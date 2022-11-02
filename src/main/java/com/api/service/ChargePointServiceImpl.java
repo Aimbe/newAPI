268e2e6437ad2f4907f045b6e9bea70c6a4666d3
qkr2435 <qkr2435@gmail.com>
2022-11-03 00:01:02 +0900

    ChargePointService implements 후 override

--- src/main/java/com/api/service/ChargePointServiceImpl.java
+++ src/main/java/com/api/service/ChargePointServiceImpl.java
@@ -1,4 +1,27 @@
 package com.api.service;
 
+import com.api.entity.ChargePointEntity;
+
+import java.util.Optional;
+
 public class ChargePointServiceImpl implements ChargePointService {
+
+    @Override
+    public void createChargePoint() {
+
+    }
+    @Override
+    public void deleteChargePointById(String id) {
+
+    }
+
+    @Override
+    public Iterable<ChargePointEntity> getAllChargerPoint() {
+        return null;
+    }
+
+    @Override
+    public Optional<ChargePointEntity> getChargePointById() {
+        return Optional.empty();
+    }
 }
